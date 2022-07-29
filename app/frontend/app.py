9cbaf58917dcbbace8a65e81f0f737c1f8a4134b
rafael-ariascalles <rafael-ariascalles@github.com>
2022-07-29 12:59:39 -0600

    <code> frontend and backend integration

--- app/frontend/app.py
+++ app/frontend/app.py
@@ -2,37 +2,42 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-st.set_page_config(page_title="GLG",layout="wide")
+import requests
+
+st.set_page_config(page_title="GLG",layout="wide",initial_sidebar_state="expanded")
 
 st.markdown(""" <style>
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 </style> """, unsafe_allow_html=True)
 
-
 padding = 0
 st.markdown(f""" <style>
-    .reportview-container .main .block-container{{
-        padding-top: {padding}rem;
-        padding-right: {padding}rem;
-        padding-left: {padding}rem;
-        padding-bottom: {padding}rem;
-    }} </style> """, unsafe_allow_html=True)
-
-st.subheader("Request Analyser")
+.reportview-container .main .block-container{{
+    padding-top: {padding}rem;
+    padding-right: {padding}rem;
+    padding-left: {padding}rem;
+    padding-bottom: {padding}rem;
+}} </style> """, unsafe_allow_html=True)
 
 
-form = st.form(key="Processing")
+st.sidebar.title("GLG")
+st.sidebar.subheader("Request Analyser")
+submited_text = st.sidebar.text_area("Customer Request:", value="",height=350, max_chars=1_000, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
 
-with form:
-    t = st.text_area("Customer Request:", value="",height=50, max_chars=None, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
-    submited = st.form_submit_button("Submit")
+if st.sidebar.button("Submit"):
+    data = {"text": submited_text}
+    response = requests.post("http://<YOUR_IP_HERE>:9898/predict", json=data)
+    response_object = response.json()
 
-st1, st2, st3, st4 = st.columns(4)
-if submited:
-    st.text(t)
-
-# write a title in H1 tag and a subtitle in H2 tag. with the title "Application"
-# write a button that when clicked, will generate a text
-# Component for uploading a jsonl file
-#uploaded_file = st.file_uploader("Upload a file", type=["jsonl"])
+    #validate response code and content
+    if response.status_code == 200:
+        st.subheader("Customer Request")
+        st.write(submited_text)
+        st1, st2 = st.columns(2)    
+        st1.subheader("Entities")
+        st1.write(response_object["entities"])
+        st2.subheader("Topics")
+        st2.write(response_object["topics"])
+    else:
+        st.write("Error:", "API not responding")
